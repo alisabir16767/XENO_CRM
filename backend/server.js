af154f092dec78e142b10a6a8761ddafd678a909
arshdhillon8560 <arshdhillon8560@gmail.com>
2025-06-03 17:07:07 +0530

    trust proxy

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@ const campaignRoutes = require("./routes/campaignRoutes");
 const communicationLogRoutes = require("./routes/communicationLogRoutes");
 
 const app = express();
+app.set("trust proxy", 1);
 const PORT = process.env.PORT || 5000;
 
 app.use(
