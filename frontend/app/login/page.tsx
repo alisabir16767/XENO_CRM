86e5103791339a3366e9d22faf2b4a26efab36b8
Sabir Ali <138771316+alisabir16767@users.noreply.github.com>
2025-06-06 18:55:06 +0530

    Update page.tsx

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -12,21 +12,17 @@ export default function LoginPage() {
 
   return (
     <div className="min-h-screen bg-[#1b1c1d] text-white relative px-6 py-8 overflow-hidden">
-      {/* Top-Left Logo */}
       <div className="absolute top-6 left-6">
         <h1 className="text-3xl font-bold text-[#A9DFD8]">GenCRM</h1>
       </div>
 
-      {/* Main Content */}
       <div className="flex flex-col lg:flex-row items-center justify-center gap-20 max-w-7xl mx-auto mt-20">
-        {/* Left: Illustration + Text */}
         <div className="flex flex-col items-center lg:items-start text-center lg:text-left max-w-xl mr-25">
           <h2 className="text-2xl lg:text-4xl font-semibold leading-tight mb-6">
             Drive Engagement with <br className="hidden lg:block" />
             <span className="text-[#A9DFD8]">Intelligence and Ease.</span>
           </h2>
 
-          {/* Image with Zoom-out and Shadow */}
           <div className="relative w-[400px] h-[300px] md:w-[500px] md:h-[400px] mt-4 rounded-2xl overflow-hidden ">
             <Image
               src="/chart-illustration.png"
@@ -37,11 +33,10 @@ export default function LoginPage() {
           </div>
         </div>
 
-        {/* Right: Login Card */}
         <Card className="bg-[#121212] shadow-2xl shadow-black/60 w-full max-w-sm rounded-2xl border border-gray-700 mt-20" >
           <CardContent className="p-8">
             <h3 className="text-2xl font-bold text-[#A9DFD8] mb-6 text-center">
-              Login to Xeno CRM
+              Login to GenCRM
             </h3>
             <Button
               onClick={handleGoogleLogin}
